d6c82062f6c45218cf09e1901bb34cd0025bad65
Princewill <duruprincewilluzochukwu@gmail.com>
2024-06-18 15:45:27 -0700

    updates ft: recent orders, cards features, chat, notification, deliveries

--- constants/app.ts
+++ constants/app.ts
@@ -1,4 +1,2 @@
-export const PROJECT_ID = "97250912-3933-43d2-addb-1085a6849ecd"
-export const GOOGLE_PLACES_API_KEY = "AIzaSyCwBek1VbADBzfIdYFW0R6UQmCoogeqyoc"
-export const PUSHER_API_KEY = "fbuopzuamzxyjydgetkb"
+
 export const CLUSTER_NAME = ""
